a29f62f7de5a87907c838f58df2997d1883d0838
saraiwr <117382906+sara11211@users.noreply.github.com>
2024-09-06 21:02:57 +0200

    project setup

---
+++ src/components/WeAreDifferent.jsx
@@ -0,0 +1,7 @@
+const WeAreDifferent = () => {
+  return (
+    <div>WeAreDifferent</div>
+  )
+}
+
+export default WeAreDifferent
---
+++ src/images/index.js
@@ -0,0 +1,30 @@
+import logo from "./logo.svg";
+
+import patternFooterDesktop from "./bg-pattern-footer-desktop.svg";
+import patternFooterMobile from "./bg-pattern-footer-mobile.svg";
+
+import patternHowWeWorkDesktop from "./bg-pattern-how-we-work-desktop.svg";
+import patternHowWeWorkMobile from "./bg-pattern-how-we-work-mobile.svg";
+
+import patternIntroLeftDesktop from "./bg-pattern-intro-left-desktop.svg";
+import patternIntroLeftMobile from "./bg-pattern-intro-left-mobile.svg";
+
+import patternIntroRightDesktop from "./bg-pattern-intro-right-desktop.svg";
+import patternIntroRightMobile from "./bg-pattern-intro-right-mobile.svg";
+
+import patternMobileNav from "./bg-pattern-mobile-nav.svg"
+
+import pinterestIcon from "./icon-pinterest.svg";
+import twitterIcon from "./icon-twitter.svg";
+import facebookIcon from "./icon-facebook.svg";
+import instagramIcon from "./icon-instagram.svg";
+
+import closeIcon from "./icon-close.svg";
+import hamburgerIcon from "./icon-hamburger.svg";
+
+import affordablePricesIcon from "./icon-affordable-prices.svg";
+import peopleFirstIcon from "./icon-people-first.svg";
+import snappyProcessIcon from "./icon-snappy-processs.svg"
+
+import imageIntroDesktop from "./image-intro-desktop.jpg";
+import imageIntroMobile from "./image-intro-mobile.jpg";
